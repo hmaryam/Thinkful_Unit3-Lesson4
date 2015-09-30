a2cfd26e4d9ab1d75a978b566bdbd746d9005def
hmaryam <hmaryam@bgsu.edu>
2015-09-29 20:31:45 -0400

    Delete config.py

--- blog/config.py
+++
@@ -1,5 +0,0 @@
-import os
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
-    DEBUG = True
-    SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "abcde")
