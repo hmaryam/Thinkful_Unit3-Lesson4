49fcadd443238b9594275284959ffa7a4bef9ddd
Maryam <hmaryam@bgsu.edu>
2015-10-04 19:45:49 -0400

    new change

--- blog/config.py
+++ blog/config.py
@@ -4,6 +4,6 @@ class DevelopmentConfig(object):
     DEBUG = True
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://hmaryam:thinkful@localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
